dba9682e17b42d416a23aac0aea21c0af0eea48b
Duy <qduy88@gmail.com>
2020-04-19 13:04:47 +0700

    Update

--- python/app.py
+++ python/app.py
@@ -3,6 +3,10 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+@app.route('/v1/app')
+def hello_world():
+    return 'Hello, World!'
+
 @app.route('/app')
 def hello_world():
     return 'Hello, World!'
